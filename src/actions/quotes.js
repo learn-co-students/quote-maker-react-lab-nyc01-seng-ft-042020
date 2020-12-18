2ea69ad4a53357658f1d301632721141886fd922
carolineglass <carolineglass89@gmail.com>
2020-12-17 19:29:04 -0500

    Done.

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,7 @@
 // TODO: Create action creators as defined in tests
+export const addQuote = newQuote => {
+    return {
+    type: "ADD_QUOTE",
+    quote: newQuote
+    };
+  }
--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -1,38 +1,50 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const QuoteCard = (props) =>
-  <div>
-    <div className="card card-inverse card-success card-primary mb-3 text-center">
-      <div className="card-block">
-        <blockquote className="card-blockquote">
-          {/* <p>{Render Quote Content}</p> */}
-          {/* <footer>- author <cite title="Source Title">{Render Quote Author}</cite></footer> */}
-        </blockquote>
-      </div>
-      <div className="float-right">
-        <div className="btn-group btn-group-sm" role="group" aria-label="Basic example">
-          <button
-            type="button"
-            className="btn btn-primary"
-          >
-            Upvote
-          </button>
-          <button
-            type="button"
-            className="btn btn-secondary"
-          >
-            Downvote
-          </button>
-          <button
-            type="button"
-            className="btn btn-danger"
-          >
-            <span aria-hidden="true">&times;</span>
-          </button>
+const QuoteCard = (props) => {
+console.log(props)
+return (
+    <div>
+      <div className="card card-inverse card-success card-primary mb-3 text-center">
+        <div className="card-block">
+          <blockquote className="card-blockquote">
+            <p>{props.content}</p>
+            <footer>- author <cite title="Source Title">{props.author}</cite></footer>
+          </blockquote>
         </div>
-        {/* <div>Votes: {Render Quote Votes}</div> */}
+        <div className="float-right">
+          <div className="btn-group btn-group-sm" role="group" aria-label="Basic example">
+            <button
+              type="button"
+              className="btn btn-primary"
+            >
+              Upvote
+            </button>
+            <button
+              type="button"
+              className="btn btn-secondary"
+            >
+              Downvote
+            </button>
+            <button
+              type="button"
+              className="btn btn-danger"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          {/* <div>Votes: {Render Quote Votes}</div> */}
+        </div>
       </div>
     </div>
-  </div>;
+)
+}
 
-export default QuoteCard;
+const mapStateToProps = state => {
+  return {
+    content: state.content,
+    author: state.author
+  }
+}
+
+export default connect(mapStateToProps)(QuoteCard);
